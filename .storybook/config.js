4a480b5ea2851c273f6e8edd017fdbfa2b90952a
Ahana Ghosh <ahanaghosh94@gmail.com>
2019-09-30 10:26:45 -0400

    Create an object called dayClass that applies the day-list__item--selected class name if props.selected is true and the day-list__item--full class name if props.spots is 0.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
 import { configure } from "@storybook/react";
+// import { configureActions } from '@storybook/addon-actions';
+
+// configureActions({
+  // depth: 100,
+  // Limit the number of items logged into the actions panel
+  // limit: 20,
+// })
 
 function loadStories() {
   require("../stories");
