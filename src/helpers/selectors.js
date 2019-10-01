a732038e311fd6c06ebc41de9d6c4fd91ef1c185
Ahana Ghosh <ahanaghosh94@gmail.com>
2019-10-01 11:39:47 -0400

    create a function called getAppointmentsForDay that will receive two arguments state and day. The function will return an array of appointments for the given day.

---
+++ src/helpers/selectors.js
@@ -0,0 +1,11 @@
+export function getAppointmentsForDay(state, day) {
+  let filteredDays = [];
+  for (let days of state.days) {
+    if (days.name === day) {
+      for (let appointment of days.appointments) {
+        filteredDays.push(state.appointments[appointment]);
+      }
+    }
+  }
+  return filteredDays;
+}
